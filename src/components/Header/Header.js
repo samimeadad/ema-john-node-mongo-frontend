da60907df3f9d59734d261433d98dab2fb8b1a56
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-27 22:58:49 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import logo from '../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    const { user, logOut } = useAuth();
+
+    return (
+        <div className="header">
+            <img className="logo" src={ logo } alt="" />
+            <nav>
+                <NavLink to="/shop">Shop</NavLink>
+                <NavLink to="/review">Order Review</NavLink>
+                <NavLink to="/inventory">Manage Inventory</NavLink>
+                <NavLink to="/register">Register</NavLink>
+                { user.displayName && <span style={ { color: 'yellow', marginRight: '5px' } }>Hello { user.displayName }</span> }
+                {
+                    user.displayName ?
+                        <button onClick={ logOut }>Logout</button>
+                        :
+                        <NavLink to="/login">Login</NavLink>
+                }
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import './Login.css';
+
+const Login = () => {
+    const { signInUsingGoogle } = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirect_uri = location.state?.from || '/';
+
+    const handleGoogleLogin = () => {
+        signInUsingGoogle()
+            .then( result => {
+                history.push( redirect_uri );
+            } )
+    }
+
+    return (
+        <div className="login-form">
+            <div>
+                <h2>Login</h2>
+                <form>
+                    <input type="email" placeholder="Your Email" />
+                    <br />
+                    <input type="password" placeholder="Password" />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>New to ema-john website? <Link to="/register">Create Account</Link> </p>
+                <div>-------- OR --------</div>
+                <br />
+                <button onClick={ handleGoogleLogin } className="btn-regular">Google Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
